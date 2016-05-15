1061392a2a6e65528f595642242ca5a56be62ec2
Erik <erikclaesroos@gmail.com>
2016-05-15 15:32:03 +0200

    Forgot to add cylinderApproach

---
+++ cylinderApproach.ino
@@ -0,0 +1,34 @@
+void cylinderApproach()
+{
+  switch(state)
+  {
+      case 0: //Approach the cylinder
+        if (targetDistance > dangerZone){
+  
+          turn(targetHeading);
+          driveDistance(targetDistance - dangerZone-10); 
+          state = 1; 
+        } else {
+  
+          turn(targetHeading);
+          driveDistance(targetDistance - 10); 
+          state = 1;
+        }
+        break;
+  
+      case 2: //Wait during cylinder approach
+  
+        if (drivingFinished) {
+          state = 3;
+        }
+        break;
+  
+      case 4: //Verify that the cylinder is grabbed
+        state = 4;
+        break;
+  
+      case 13:
+        break;
+  
+  }
+}
